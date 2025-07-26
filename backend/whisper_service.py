898e61a5425d0fb40be3620cec934553424130d9
Deflat <46375636+D3FLAT@users.noreply.github.com>
2025-07-27 01:11:27 +0400

    Delete backend/whisper_service.py
    Лишний файл

--- backend/whisper_service.py
+++
@@ -1,6 +0,0 @@
-import whisper
-
-def transcribe_audio(file_path: str) -> str:
-    model = whisper.load_model("base")
-    result = model.transcribe(file_path, language="ru")
-    return result["text"]
